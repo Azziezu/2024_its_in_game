e9f4f98368392e9361d0db8e43f9a2255a8655d6
Bart Vegter <github@bartvegter.com>
2024-11-28 23:45:10 +0100

    Implementatie van spelschermen. Pas gameState aan in init() om te testen.

--- src/BasicGame.java
+++ src/BasicGame.java
@@ -1,3 +1,4 @@
+import nl.saxion.app.CsvReader;
 import nl.saxion.app.SaxionApp;
 
 import nl.saxion.app.interaction.GameLoop;
@@ -6,8 +7,14 @@ import nl.saxion.app.interaction.MouseEvent;
 
 public class BasicGame implements GameLoop {
 
+    static final int STARTSCREEN = 1;
+    static final int GAMESCREEN = 2;
+    static final int ENDSCREEN = 3;
+
     int x, y; // Muiscoördinaten
-    String raadwoord = "Capybara";
+    String raadwoord;
+    int gameState;
+
 
     public static void main(String[] args) {
         SaxionApp.startGameLoop(new BasicGame(), 1000, 1000, 40);
@@ -15,10 +22,10 @@ public class BasicGame implements GameLoop {
 
     @Override
     public void init() { // Voert eenmalig dingen uit aan het begin van het spel
-      SaxionApp.printLine(spelernaam);
-
+        raadwoord = "Capybara";
+        gameState = STARTSCREEN;
+        // SaxionApp.printLine(spelernaam);
     }
-
 
     // Slaat invoer van de spelernaam op
     public static String spelernaam;
@@ -26,21 +33,44 @@ public class BasicGame implements GameLoop {
         SaxionApp.printLine(" Voer je naam: ");
         String spelernaam = SaxionApp.readString();
         return spelernaam;
-
     }
 
     @Override
     public void loop() {  // Blijft dingen herhalen gedurende het spel
         SaxionApp.clear();
-        SaxionApp.drawText("Voer een letter in: ", 5, 5, 16);
-
-
+        switch (gameState) {
+            case STARTSCREEN:
+                drawStartScreen();
+                break;
+            case GAMESCREEN:
+                drawGameScreen();
+                break;
+            case ENDSCREEN:
+                drawEndScreen();
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
     public void keyboardEvent(KeyboardEvent keyboardEvent) { // Herkent invoer op toetsenbord
         if (keyboardEvent.isKeyPressed()) {
-            // if (keyboardEvent.getKeyCode() == KeyboardEvent.VK_SPACE) {}
+            switch (gameState) {
+                case STARTSCREEN:
+                    if (keyboardEvent.getKeyCode() == KeyboardEvent.VK_SPACE) {
+                        gameState = GAMESCREEN;
+                    }
+                    break;
+                case GAMESCREEN:
+                    //
+                    break;
+                case ENDSCREEN:
+                    //
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
@@ -48,6 +78,19 @@ public class BasicGame implements GameLoop {
     public void mouseEvent(MouseEvent mouseEvent) { // Herkent invoer op muis
         x = mouseEvent.getX();
         y = mouseEvent.getY();
+    }
+
+    public void drawStartScreen() {
+        SaxionApp.drawText("Laat het hoofd niet rollen!", 20, 20, 24);
+        SaxionApp.drawText("Druk op spatie om het spel te starten", 20, 60, 24);
+    }
+
+    public void drawGameScreen() {
+        SaxionApp.drawText("Voer een letter in: ", 5, 5, 16);
+    }
+
+    public void drawEndScreen() {
+        //
     }
 
 }
